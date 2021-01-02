779742d9f36c156f8a72852fb61bbbeb9a187a1e
renato junior <momentum323@gmail.com>
2021-01-02 13:13:06 -0300

    mobx

---
+++ lib/mobx/counter.dart
@@ -0,0 +1,14 @@
+import 'package:mobx/mobx.dart';
+part 'counter.g.dart';
+
+class Counter = CounterBase with _$Counter;
+
+abstract class CounterBase with Store {
+  @observable
+  int count = 0;
+
+  @action
+  void increment() {
+    count++;
+  }
+}
---
+++ lib/page/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import '../mobx/counter.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var counter = Counter();
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(
+              builder: (_) {
+                print('observer');
+                return Text(
+                  '${counter.count}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counter.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
